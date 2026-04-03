928c8a51cc31701a548cc87bc614604a2db63afe
Viktor Kondratsiuk <3334976@gmail.com>
2026-04-03 17:56:51 +0300

    refactoring

--- src/collections/Customers.ts
+++ src/collections/Customers.ts
@@ -1,14 +1,10 @@
 import { CollectionConfig } from 'payload';
-import { afterLogin } from '@/hooks/afterLogin';
 
 export const Customers: CollectionConfig = {
   slug: 'customers',
   auth: true,
   access: {
     create: () => true,
-  },
-  hooks: {
-    afterLogin: [afterLogin],
   },
   fields: [
     { name: 'firstName', type: 'text' },
